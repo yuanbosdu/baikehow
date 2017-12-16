540160167633e1f8f2d299c8fef5a2224823e19b
Yuanbo <yuanbosdu@163.com>
2017-12-16 19:00:36 +0800

    [howbaike] add the macro "QR"
    Signed-off-by: Yuanbo <yuanbosdu@163.com>

---
+++ MoinMoin/macro/QR.py
@@ -0,0 +1,31 @@
+import os
+from subprocess import Popen, PIPE
+from hashlib import sha1
+from MoinMoin.Page import Page
+
+qr_options = ["-s", "4"]
+
+def png_path(request, pagename, text):
+        return "/home/baikehow/moin/wiki/data/qr", png_filename(text)
+def png_url(request, pagename, text):
+        return "/data/qr/" + png_filename(text)
+def png_filename(text):
+        return sha1(text).hexdigest()[:16] + ".png"
+
+def macro_QR(macro, text=None):
+        if text is None:
+                text = macro.request.getQualifiedURL(macro.request.page.url(macro.request))
+
+        prefix, suffix = png_path(macro.request, macro.request.page.page_name, text)
+        if not os.path.exists(prefix):
+                os.mkdir(prefix)
+
+        filename = os.path.join(prefix, suffix)
+        if not os.path.exists(filename):
+                p = Popen(["qrencode", "-o", filename] + qr_options, stdin=PIPE)
+                p.stdin.write(text)
+                p.stdin.close()
+                p.wait()
+
+        img_url = png_url(macro.request, macro.request.page.page_name, text)
+        return macro.formatter.image(img_url, alt=text, title=text)
